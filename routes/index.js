6dd32096e7372c9af7120a049a3c8e4a1573bd8e
andresPirona <andrespirona92@gmail.com>
2017-09-28 17:08:32 -0500

    prueba 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'probando Mensappjeros' });
+  res.render('index', { title: 'probando1 Mensappjeros' });
 });
 
 router.get('/hola', function(req, res, next) {
