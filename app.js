3de007e55989d91d0d32f904fe5676128440aeb1
andresPirona <andrespirona92@gmail.com>
2017-09-28 14:21:53 -0500

    NodeJS 28/09

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 /// USE ROUTES
 app.use('/', index);
 app.use('/users', users);
