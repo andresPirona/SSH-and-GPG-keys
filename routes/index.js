b601f3a45fb1f162100ee62d4054de2b7afa27f4
andresPirona <andrespirona92@gmail.com>
2017-09-28 18:05:03 -0500

    desde local con ssh

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'probandossh  Mensappjeros' });
+  res.render('index', { title: 'probandossh ahora http  Mensappjeros' });
 });
 
 router.get('/hola', function(req, res, next) {
