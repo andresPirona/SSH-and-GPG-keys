090fe7132ef08559464d6af0c196c0d843d23bab
Ubuntu <ubuntu@ip-172-31-41-221.us-west-2.compute.internal>
2017-09-28 17:52:16 +0000

    hola mundo desde el server con ssh

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'probandofttt  Mensappjeros' });
+  res.render('index', { title: 'probandossh  Mensappjeros' });
 });
 
 router.get('/hola', function(req, res, next) {
